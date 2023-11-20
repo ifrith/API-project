487442f25b93590b9bb07fefee4761c4d8b8746e
ifrith <ifrith@deloitte.com.au>
2023-11-20 22:48:31 +1100

    added

--- app/core/tests/tests_admin.py
+++ app/core/tests/tests_admin.py
@@ -34,9 +34,9 @@ class AdminSiteTests(TestCase):
 
         self.assertEqual(res.status_code, 200)
     
-    def create_user_page(self):
+    def test_create_user_page(self):
         """test the create user page works"""
-        url = reverse('admincore_user_add')
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
