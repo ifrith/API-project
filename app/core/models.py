dc2a3ee725be374b65a010708ba5cb5e75d2a6e4
ifrith <ifrith@deloitte.com.au>
2023-11-20 15:53:59 +1100

    added tests

--- app/core/models.py
+++ app/core/models.py
@@ -10,15 +10,26 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users"""
 
-    def create_user(self, email, password=none, **extra_field):
+    def create_user(self, email, password=None, **extra_field):
         """create save and return a new user"""
-        user = self.mode(email=email, **extra_field)
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
 
         return user 
 
 
+    def create_superuser(self, email, password):
+        """create and return a new superuser"""
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user 
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -27,5 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
